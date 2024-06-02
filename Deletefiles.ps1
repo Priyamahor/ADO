e94709f597ca1593bea2e00d383442783e367fa6
Priya <Priyamahor47@GMAIL.COM>
2024-06-02 15:18:29 +0530

    Delete files add

---
+++ Deletefiles.ps1
@@ -0,0 +1,25 @@
+param(
+[string]$targetDirectory,
+[string]$filePattern
+)
+
+if(-not (Test-Path -Path &targetDirectory))
+{
+Write-Error "the directory'$targetDirectory' does not exist'
+exit 1
+}
+
+$files = Get-childitem -Path $targetDirectory
+-Filter $filePattern
+
+foreach ($file in $files){
+try {
+Remove-Item -Path $file.FullName -Force
+Write-Output "Deleted file: &($file.FullName)"
+}
+catch
+{
+Write-Error "failed to delete file: $($file.FullName). Error:$_"
+}
+}
+
